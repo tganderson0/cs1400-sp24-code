f4a79f824be612c8f824eb092f92e91cf9a6fc3e
Taylor Anderson <tganderson0@gmail.com>
2024-01-10 06:52:22 -0700

    fixed some of the code for class today

--- unit1/1-10-24/drawly-shapes.py
+++ unit1/1-10-24/drawly-shapes.py
@@ -2,13 +2,15 @@ import drawly
 
 drawly.start("My Drawing Program", background="grey")
 
+drawly.set_speed(3)
+
 drawly.circle(300, 300, 150)
 drawly.set_color("blue")
 drawly.circle(600, 200, 100)
 drawly.draw()
 
 drawly.set_color("black")
-drawly.ring(600, 200, 100, 10)
+drawly.circle(600, 200, 100, 10)
 drawly.draw()
 
 drawly.done()
